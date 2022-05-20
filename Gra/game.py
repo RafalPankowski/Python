7463715eca8ba8a06e20fc8725f3080462292af2
RafalPankowski.lap <rafal.pankowski.pks@gmail.com>
2022-05-20 11:19:19 +0200

    Sorting code

--- Gra/game.py
+++ Gra/game.py
@@ -10,21 +10,25 @@ clock = pygame.time.Clock()
 test_font = pygame.font.Font(None, 50) # type font, size
 game_active = True
 
+#Design
 background_surface = pygame.Surface((800,800))
 background_surface.fill('Black')
 
 sky_surface = pygame.Surface((800,50))
 sky_surface.fill('Blue')
-#ladowanie obrazu: pygame.image.load('sciezka obrazu')
+
 ground_surface = pygame.Surface((800,100))
 ground_surface.fill('Green')
 
+#Text
 score_surf = test_font.render('Score:', False, 'Brown')
 score_rect = score_surf.get_rect(center = (400, 70))
 
+#Enemy
 snail_surface = pygame.image.load('grafika\\snail.png').convert_alpha()
 snail_rect = snail_surface.get_rect(bottomright = (700,300))
 
+#Player
 player_surf = pygame.Surface((50,100))
 player_surf.fill('Red')
 player_rect = player_surf.get_rect(midbottom = (80,300))
@@ -45,7 +49,8 @@ while True:
                 if event.key == pygame.K_SPACE:
                     player_gravity = -20
     
-    if game_active:    
+    if game_active:  
+        #'Render'  
         #Kolejnosc deklarowania elementow jest bardzo wazna i trzeba byc precyzyjnym
         screen.blit(background_surface,(0,0))
         screen.blit(sky_surface,(0,0)) # x , y
@@ -54,11 +59,12 @@ while True:
         pygame.draw.rect(screen, 'Pink', score_rect, 10)
         screen.blit(score_surf,score_rect)
 
+        #Enemy attributes (Movement)
         snail_rect.x -= 6
         if snail_rect.right <= 0: snail_rect.left = 800
         screen.blit(snail_surface,snail_rect)
 
-        #Player
+        #Player attributes
         player_gravity += 1
         player_rect.y += player_gravity
         if player_rect.bottom >= 300: player_rect.bottom = 300
@@ -71,10 +77,13 @@ while True:
     else:
         screen.fill('Yellow')
         
+    #Other solutions
+        #Jump
         #keys = pygame.key.get_pressed()
         #if keys[pygame.K_SPACE]:
         # player_gravity = -10
 
+        #Mouse reaction
         #mouse_pos = pygame.mouse.get_pos()
         #if player_rect.collidepoint(mouse_pos):
         #   print('collision')

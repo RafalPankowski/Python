3aea7668fa9673cadd3df6ef5b1b73d803707835
RafalPankowski.lap <rafal.pankowski.pks@gmail.com>
2022-05-20 13:16:27 +0200

    Improved enemy respawn and collisions

--- Gra/game.py
+++ Gra/game.py
@@ -2,21 +2,41 @@ from cgi import test
 from cmath import rect
 import pygame
 from sys import exit
+from random import randint
 
 def display_score():
     current_time = int(pygame.time.get_ticks() / 1000) - start_time
     score_surf = test_font.render(f'{current_time}', False, 'White')
     score_rect = score_surf.get_rect(center = (500, 70))
     screen.blit(score_surf,score_rect)
+    return current_time
 
+def obstacle_movement(obstacle_list):
+    if obstacle_list:
+        for obstacle_rect in obstacle_list:
+            obstacle_rect.x -= 5
+
+            if obstacle_rect.bottom == 300: screen.blit(snail_surface,obstacle_rect)
+
+        obstacle_list = [obstacle for obstacle in obstacle_list if obstacle.x > -100]
+
+        return obstacle_list
+    else: return[]
+
+def collisions(player,obstacles):
+    if obstacles:
+        for obstacle_rect in obstacles:
+            if player.colliderect(obstacle_rect): return False
+    return True
 
 pygame.init()
 screen = pygame.display.set_mode((800,400)) # w ,h 
 pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
 test_font = pygame.font.Font(None, 50) # type font, size
-game_active = True
+game_active = False
 start_time = 0
+score = 0
 
 #Design
 background_surface = pygame.Surface((800,800))
@@ -36,17 +56,23 @@ game_over_surf = test_font.render('Game Over',False,'Brown')
 game_over_rect = game_over_surf.get_rect(center = (400, 70))
 
 reset_text_surf = test_font.render('Press SPACEBAR to play again',False,'Brown')
-reset_text_rect = reset_text_surf.get_rect(center = (400, 140))
+reset_text_rect = reset_text_surf.get_rect(center = (400, 210))
 
 #Enemy
 snail_surface = pygame.image.load('grafika\\snail.png').convert_alpha()
-snail_rect = snail_surface.get_rect(bottomright = (700,300))
+snail_rect = snail_surface.get_rect(bottomright = (600,300))
+
+obstacle_rect_list = []
 
 #Player
 player_surf = pygame.Surface((50,100))
 player_surf.fill('Red')
 player_rect = player_surf.get_rect(midbottom = (80,300))
 player_gravity = 0
+
+#Timer
+obstacle_timer = pygame.USEREVENT + 1 
+pygame.time.set_timer(obstacle_timer,1500)
 
 while True:
     for event in pygame.event.get():
@@ -68,6 +94,9 @@ while True:
                 snail_rect.left = 700
                 start_time = int(pygame.time.get_ticks() / 1000)
 
+        if event.type == obstacle_timer and game_active:
+            obstacle_rect_list.append(snail_surface.get_rect(bottomright = (randint(900,1100),300)))
+
     
     if game_active:  
         #'Render'  
@@ -78,12 +107,12 @@ while True:
         pygame.draw.rect(screen, 'Pink', score_text_rect)
         pygame.draw.rect(screen, 'Pink', score_text_rect, 10)
         screen.blit(score_text_surf,score_text_rect)
-        display_score()
+        score = display_score()
 
-        #Enemy attributes (Movement)
-        snail_rect.x -= 6
-        if snail_rect.right <= 0: snail_rect.left = 800
-        screen.blit(snail_surface,snail_rect)
+        #Enemy attributes (Movement) old
+        #snail_rect.x -= 6
+        #if snail_rect.right <= 0: snail_rect.left = 800
+        #screen.blit(snail_surface,snail_rect)
 
         #Player attributes
         player_gravity += 1
@@ -91,13 +120,22 @@ while True:
         if player_rect.bottom >= 300: player_rect.bottom = 300
         screen.blit(player_surf,player_rect)
 
+        #Obstacle movment
+        obstacle_rect_list = obstacle_movement(obstacle_rect_list)
+
+
         #Collision
-        if snail_rect.colliderect(player_rect):
-            game_active = False
+        game_active = collisions(player_rect,obstacle_rect_list)
 
     else:
         screen.fill('Yellow')
         screen.blit(game_over_surf,game_over_rect)
+        obstacle_rect_list.clear()
+        player_rect.midbottom = (80,300)
+        player_gravity = 0
+        score_message = test_font.render(f'Your score: {score}', False, 'Brown')
+        score_message_rect = score_message.get_rect(center = (400, 140))
+        screen.blit(score_message,score_message_rect)
         screen.blit(reset_text_surf,reset_text_rect)
         
     #Other solutions
